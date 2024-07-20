f27892e053548b2c43a53871ac1c267a3234edbf
Eric Moore <ericmoore0709@gmail.com>
2024-07-20 16:46:26 -0700

    documented sqlForPartialUpdate

--- helpers/sql.js
+++ helpers/sql.js
@@ -1,14 +1,22 @@
 const { BadRequestError } = require("../expressError");
 
-// THIS NEEDS SOME GREAT DOCUMENTATION.
-
+/**
+ * Attempts to convert data to SQL fragments for including in DB partial update queries.
+ * @param {*} dataToUpdate the data values to update
+ * @param {*} jsToSql the JS to convert to SQL
+ * @returns `setCols` string of "set X = Y" fragments for SQL update, `values` array of values from updated data
+ */
 function sqlForPartialUpdate(dataToUpdate, jsToSql) {
+  // get keys from data
   const keys = Object.keys(dataToUpdate);
+
+  // if there are no keys, throw error
   if (keys.length === 0) throw new BadRequestError("No data");
 
-  // {firstName: 'Aliya', age: 32} => ['"first_name"=$1', '"age"=$2']
+  // map each key to a string -- 'colName = $indx+1'
+  // i.e. {firstName: 'Aliya', age: 32} => ['"first_name"=$1', '"age"=$2']
   const cols = keys.map((colName, idx) =>
-      `"${jsToSql[colName] || colName}"=$${idx + 1}`,
+    `"${jsToSql[colName] || colName}"=$${idx + 1}`,
   );
 
   return {
